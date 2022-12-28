539404f830a99caccd5c3f253afe101c1d78ca3e
JadRifai19 <jad.rami.rifaii19@gmail.com>
2022-12-28 13:32:10 +0200

    step4-Let's complicate the API

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ app.listen(port, () => {
     console.log(`listening on port of ${port}`)
   })
 
+app.get ('/test', (req, res) => {
+    res.json({status:200, message:"ok"})
+})
+
+app.get('/time', (rep, res) => {
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+    const seconds = currentTime.getSeconds();
+    res.json({status:200 , message:'${hours}:${seconds}'})
+})
+
+app.get('/hello/id', (rep,res) =>{
+    res.json({ status:200, message:`Hello, ${id}`})
+})
+
+app.get('/search?s=SEARCH' , (rep, res) => {
+    const search = req.query.search    
+    if (search){
+        res.status(200).json({status:200, message:"ok", data:search})}
+    else {
+        res.status(500).json({status: 500, error:true, message:"you have to provide a search"})
+    }    
+})
