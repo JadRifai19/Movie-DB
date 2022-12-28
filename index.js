8ee1fbddf445d8ad1af82ee3c34411d9cf4112d1
JadRifai19 <jad.rami.rifaii19@gmail.com>
2022-12-28 13:45:13 +0200

    step5-Set up the basis for CRUD

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require ('express')
 const app = express()
 const port = 3000
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
 
 app.get('/' ,(req, res) => {
     res.send('ok')
@@ -33,3 +39,13 @@ app.get('/search?s=SEARCH' , (rep, res) => {
         res.status(500).json({status: 500, error:true, message:"you have to provide a search"})
     }    
 })
+
+app.get('/movies/create' , (req, res) => {})
+
+app.get('movies/read' , (req, res) => {
+    res.json({status:200, data:movies})
+})
+
+app.get('movies/edit' , (req, res) => {})
+
+app.get('movies/delete' , (req, res) => {})
