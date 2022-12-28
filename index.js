95693e4cdabb2790a5156b510946383f3f2c5b59
JadRifai19 <jad.rami.rifaii19@gmail.com>
2022-12-28 11:20:03 +0200

    step3-Create an express simple API

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require ('express')
+const app = express()
+const port = 3000
+
+app.get('/' ,(req, res) => {
+    res.send('ok')
+})
+
+app.listen(port, () => {
+    console.log(`listening on port of ${port}`)
+  })
+
+app.get ('/test', (req, res) => {
+    res.json({status:200, message:"ok"})
+})
+
+app.get('/time', (rep, res) => {
+    const currentTime = new Date();
+    const hours = currentTime.getHours();
+    const seconds = currentTime.getSeconds();
+    res.json({status:200 , message:'${hours}:${seconds}'})
+})
